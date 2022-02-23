ad0030f9b620e14e4ccf33a06c4a484447547608
Guindo Mouctar Ousseini <mouctar-ousseini.guindo@etu.univ-orleans.fr>
2022-02-23 15:19:07 +0100

    config heroku

--- serveur.js
+++ serveur.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + '/dist/arsat-client'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*' , function (req, res)
 {
-  res.sendfile(path.join(__dirname+'/dist/arsat-client/index.html'));
+  res.sendfile(path.join(__dirname+'/dist/index.html'));
 })
 app.listen(process.env.PORT);
